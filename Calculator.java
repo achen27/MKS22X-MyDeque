3b99ebd5f236c124ce1ef5bbe5ba416277537f87
Amanda Chen <achen01@stuy.edu>
2019-03-25 10:56:31 -0400

    added numbers to stack

--- Calculator.java
+++ Calculator.java
@@ -6,6 +6,15 @@ public class Calculator{
   public static double eval(String s){
     String[] tokens = s.split(" ");
     System.out.println(Arrays.toString(tokens));
+    MyDeque<Double> d = new MyDeque<Double>();
+    for (int i = 0; i < tokens.length; i++){
+      try{
+        d.addLast(Double.parseDouble(tokens[i]));
+      }catch (NumberFormatException e){
+
+      }
+    }
+    System.out.println(d.toStringDebug());
     return 0.0;
   }
 
