e7beb47521eb9bb4acee5b89b20dfd4ae361f0d7
Amanda Chen <achen01@stuy.edu>
2019-03-24 21:11:59 -0400

    fixed resize

--- MyDeque.java
+++ MyDeque.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MyDeque<E>{
   private E[] data;
-  private int size, start, end;
+  public int size, start, end;
 
   public MyDeque(){
     @SuppressWarnings("unchecked")
@@ -30,19 +30,47 @@ public class MyDeque<E>{
     @SuppressWarnings("unchecked")
     E[] newData = (E[])new Object[size*2];
     int idx = 0;
-    for (int i = start; i != end+1; i++){
+    //System.out.println("S "+start);
+    //System.out.println("E "+end);
+    /*for (int i = start; i != end; i++){
+      System.out.println(i != end);
+      System.out.println("I "+i);
+      System.out.println("E "+end);
+      System.out.println("data: "+ Arrays.toString(newData));
       if (i == data.length){
         i = 0;
       }
+      //System.out.println("I "+i);
       newData[idx] = data[i];
       idx++;
     }
+    //newData[idx] = data[end];*/
+    if (end > start){
+      for (int i = start; i <= end; i++){
+        //System.out.println("data[i]1 "+ data[i]);
+        newData[idx] = data[i];
+        idx++;
+      }
+    }else if (end < start){
+      for (int i = start; i < data.length; i++){
+        //System.out.println("data[i]2 "+ data[i]);
+        newData[idx] = data[i];
+        idx++;
+      }
+      for (int i = 0; i <= end; i++){
+        //System.out.println("data[i]3 "+ data[i]);
+        newData[idx] = data[i];
+        idx++;
+      }
+    }
+    start = 0;
+    end = size - 1;
     data = newData;
   }
 
   public String toString(){
     String output = "{";
-    System.out.println("start: "+start+" end: "+end);
+    //System.out.println("start: "+start+" end: "+end);
     if (size != 0){
       if (end > start || size == 1){
         for (int i = start; i <= end; i++){
@@ -62,6 +90,7 @@ public class MyDeque<E>{
       }
     }
     output += "}";
+    //System.out.println(toStringDebug());
     return output;
   }
 
@@ -78,15 +107,20 @@ public class MyDeque<E>{
     if (element == null){
       throw new NullPointerException("Element is Null");
     }
-    if (size == data.length){
-      resize();
-    }
-    if (start == 0){
-      start = data.length-1;
+    if (size == 0){
+      data[start] = element;
+      //System.out.println(data[end]);
     } else {
-      start--;
+      if (size == data.length){
+        resize();
+      }
+      if (start == 0){
+        start = data.length-1;
+      } else {
+        start--;
+      }
+      data[start] = element;
     }
-    data[start] = element;
     size++;
   }
 
@@ -159,13 +193,17 @@ public class MyDeque<E>{
 
   public static void main(String[] args){
     MyDeque<Integer> d = new MyDeque<Integer>();
-    System.out.println(d.toString());
-    for(int i =0; i < 11; i++){
-      d.addLast(i);
+    //System.out.println(d.toString());
+    for(int i =0; i < 21; i++){
+      d.addFirst(i);
+      System.out.println("S: "+d.start);
+      System.out.println("E: "+d.end);
+      System.out.println("Debug: " +d.toStringDebug());
       System.out.println(d.toString());
     }
+    //d.addFirst(0);
     //System.out.println(d.toString());
-    //System.out.println(d.toStringDebug());
+    System.out.println(d.toStringDebug());
 
     //System.out.println(d.toString());
     //d.addFirst("hello");
